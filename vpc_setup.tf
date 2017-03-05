b3928a9fc2101b9fc87b067cf7c5f5bdf8d58cab
Ang Kah Min, Kelvin <syraxius@hotmail.com>
2017-03-05 14:12:50 +0800

    Modified to suit Singapore settings.

--- vpc_setup.tf
+++ vpc_setup.tf
@@ -19,7 +19,7 @@ resource "aws_internet_gateway" "main-gw" {
     }
 }
 
-# route tables
+# Route Tables
 resource "aws_route_table" "main-public" {
     vpc_id = "${aws_vpc.main.id}"
     route {
@@ -28,7 +28,7 @@ resource "aws_route_table" "main-public" {
     }
 
     tags {
-        Name = "main-public-1"
+        Name = "main-public"
     }
 }
 
@@ -37,7 +37,7 @@ resource "aws_subnet" "main-public-1" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true"
-    availability_zone = "eu-west-1a"
+    availability_zone = "ap-southeast-1a"
 
     tags {
         Name = "main-public-1"
@@ -47,27 +47,17 @@ resource "aws_subnet" "main-public-2" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.2.0/24"
     map_public_ip_on_launch = "true"
-    availability_zone = "eu-west-1b"
+    availability_zone = "ap-southeast-1b"
 
     tags {
         Name = "main-public-2"
     }
 }
-resource "aws_subnet" "main-public-3" {
-    vpc_id = "${aws_vpc.main.id}"
-    cidr_block = "10.0.3.0/24"
-    map_public_ip_on_launch = "true"
-    availability_zone = "eu-west-1c"
-
-    tags {
-        Name = "main-public-3"
-    }
-}
 resource "aws_subnet" "main-private-1" {
     vpc_id = "${aws_vpc.main.id}"
-    cidr_block = "10.0.4.0/24"
+    cidr_block = "10.0.3.0/24"
     map_public_ip_on_launch = "false"
-    availability_zone = "eu-west-1a"
+    availability_zone = "ap-southeast-1a"
 
     tags {
         Name = "main-private-1"
@@ -75,34 +65,21 @@ resource "aws_subnet" "main-private-1" {
 }
 resource "aws_subnet" "main-private-2" {
     vpc_id = "${aws_vpc.main.id}"
-    cidr_block = "10.0.5.0/24"
+    cidr_block = "10.0.4.0/24"
     map_public_ip_on_launch = "false"
-    availability_zone = "eu-west-1b"
+    availability_zone = "ap-southeast-1b"
 
     tags {
         Name = "main-private-2"
     }
 }
-resource "aws_subnet" "main-private-3" {
-    vpc_id = "${aws_vpc.main.id}"
-    cidr_block = "10.0.6.0/24"
-    map_public_ip_on_launch = "false"
-    availability_zone = "eu-west-1c"
 
-    tags {
-        Name = "main-private-3"
-    }
-}
-# route associations public
+# Route Associations Public
 resource "aws_route_table_association" "main-public-1-a" {
     subnet_id = "${aws_subnet.main-public-1.id}"
     route_table_id = "${aws_route_table.main-public.id}"
 }
 resource "aws_route_table_association" "main-public-2-a" {
     subnet_id = "${aws_subnet.main-public-2.id}"
-    route_table_id = "${aws_route_table.main-public.id}"
-}
-resource "aws_route_table_association" "main-public-3-a" {
-    subnet_id = "${aws_subnet.main-public-3.id}"
     route_table_id = "${aws_route_table.main-public.id}"
 }
