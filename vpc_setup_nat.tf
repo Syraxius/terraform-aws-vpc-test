ff4376fe2554201ba647235a08219f91e615f8a6
Ang Kah Min, Kelvin <syraxius@hotmail.com>
2017-03-05 15:17:11 +0800

    Removed the third private network from the nat setup

--- vpc_setup_nat.tf
+++ vpc_setup_nat.tf
@@ -9,16 +9,13 @@ resource "aws_route_table" "main-private" {
         Name = "main-private-1"
     }
 }
-# route associations private
+
+# Route Associations Private
 resource "aws_route_table_association" "main-private-1-a" {
     subnet_id = "${aws_subnet.main-private-1.id}"
     route_table_id = "${aws_route_table.main-private.id}"
 }
 resource "aws_route_table_association" "main-private-2-a" {
     subnet_id = "${aws_subnet.main-private-2.id}"
-    route_table_id = "${aws_route_table.main-private.id}"
-}
-resource "aws_route_table_association" "main-private-3-a" {
-    subnet_id = "${aws_subnet.main-private-3.id}"
     route_table_id = "${aws_route_table.main-private.id}"
 }
